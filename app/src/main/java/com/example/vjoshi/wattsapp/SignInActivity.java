0f4ebaf0eb3d204eafed154dd0a5e024edafd6a4
Eric Castronovo <ecastronovo@scu.edu>
2018-11-06 16:57:29 -0800

    Update Signin UI.

--- app/src/main/java/com/example/vjoshi/wattsapp/SignInActivity.java
+++ app/src/main/java/com/example/vjoshi/wattsapp/SignInActivity.java
@@ -10,19 +10,18 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class SignInActivity extends AppCompatActivity {
-
+    private EditText focusText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
-        EditText focusText;
         focusText = findViewById(R.id.editText2);
         focusText.requestFocus();
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
     }
     public void finish(View view) {
-        Intent intent = new Intent(this, MainList.class);
-        startActivity(intent);
+//        Intent intent = new Intent(this, MainList.class);
+//        startActivity(intent);
     }
 }
 
