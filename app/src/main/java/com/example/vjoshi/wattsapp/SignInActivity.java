682f9e06cef0311d96a6b22465dc9584399dc4d5
Vineet Joshi <vineet0713@gmail.com>
2018-11-07 14:41:02 -0800

    Add Firebase Auth for sign up page.

--- app/src/main/java/com/example/vjoshi/wattsapp/SignInActivity.java
+++ app/src/main/java/com/example/vjoshi/wattsapp/SignInActivity.java
@@ -1,27 +1,134 @@
 package com.example.vjoshi.wattsapp;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class SignInActivity extends AppCompatActivity {
-    private EditText focusText;
+    private static final String TAG = "SignInActivity";
+
+    private FirebaseAuth auth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+
+    private EditText emailField, passwordField;
+    private Button finishButton, joinButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
-        focusText = findViewById(R.id.editText2);
-        focusText.requestFocus();
-        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+
+        auth = FirebaseAuth.getInstance();
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // user is signed in!
+                    Log.d(TAG, "user is signed in!");
+                } else {
+                    // user is not signed in
+                    Log.d(TAG, "user is not signed in");
+                }
+            }
+        };
+
+        emailField = findViewById(R.id.editText2);
+        passwordField = findViewById(R.id.editText3);
+        clearTextFields();
+
+        finishButton = findViewById(R.id.button3);
+        finishButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finishPressed();
+            }
+        });
+
+        joinButton = findViewById(R.id.button4);
+        joinButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                joinPressed();
+            }
+        });
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        auth.addAuthStateListener(authStateListener);
+    }
+
     public void finish(View view) {
 //        Intent intent = new Intent(this, MainList.class);
 //        startActivity(intent);
+    }
+
+    private void finishPressed() {
+        hideKeyboard();
+
+        Log.d(TAG, "finishPressed");
+        Log.d(TAG, emailField.getText().toString());
+        Log.d(TAG, passwordField.getText().toString());
+    }
+
+    private void joinPressed() {
+        hideKeyboard();
+
+        Log.d(TAG, "joinPressed");
+
+        String email = emailField.getText().toString();
+        final String password = passwordField.getText().toString();
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Log.d(TAG, "create user successful!");
+                } else {
+                    Log.d(TAG, "create user failed: " + task.getException());
+                    displayAlert("Create User Failed", task.getException().getMessage());
+                }
+                clearTextFields();
+            }
+        });
+    }
+
+    private void hideKeyboard() {
+        InputMethodManager imm = (InputMethodManager)(getSystemService(Activity.INPUT_METHOD_SERVICE));
+        imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+    }
+
+    private void displayAlert(String title, String message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(title);
+        builder.setMessage(message);
+        builder.setPositiveButton("Ok", null);
+        builder.show();
+    }
+
+    private void clearTextFields() {
+        emailField.setText("");
+        passwordField.setText("");
+        emailField.requestFocus();
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
     }
 }
 
