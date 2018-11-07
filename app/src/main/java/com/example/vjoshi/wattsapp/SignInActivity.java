e2affe6c10e8a9b7467db886e37425916d0b0824
Eric Castronovo <ecastronovo@scu.edu>
2018-11-06 16:48:53 -0800

    Add Sign Up Activity.

---
+++ app/src/main/java/com/example/vjoshi/wattsapp/SignInActivity.java
@@ -0,0 +1,28 @@
+package com.example.vjoshi.wattsapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+        EditText focusText;
+        focusText = findViewById(R.id.editText2);
+        focusText.requestFocus();
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+    }
+    public void finish(View view) {
+        Intent intent = new Intent(this, MainList.class);
+        startActivity(intent);
+    }
+}
+
