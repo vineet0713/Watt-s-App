c34ac5e8d526b30db8d199ac2a8ae55aa7249ad6
kenwakaba <kwakaba@scu.edu>
2018-11-13 18:58:00 -0800

    ADD Device button works

--- app/src/main/java/com/example/vjoshi/wattsapp/HomeActivity.java
+++ app/src/main/java/com/example/vjoshi/wattsapp/HomeActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.GridLayout;
@@ -18,6 +19,8 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.ArrayList;
+
 import static com.example.vjoshi.wattsapp.addDeviceClasses.DeviceConstants.*;
 
 // For Redeem class
@@ -28,7 +31,7 @@ public class HomeActivity extends AppCompatActivity {
 
     private DatabaseReference database;
 
-    private ArrayList<Device> devices;
+    private ArrayList<Device> devices =  new ArrayList<>();;
 
     private static GridLayout gridLayout = null;
     private static Context context = null;
@@ -71,8 +74,6 @@ public class HomeActivity extends AppCompatActivity {
         // this is the root of the database
         database = FirebaseDatabase.getInstance().getReference();
 
-        devices = new ArrayList<>();
-
         final Intent profileIntent = new Intent(this, ProfileActivity.class);
         final Intent redeemIntent = new Intent(this, TestRedeemActivity.class);
         final Intent deviceIntent = new Intent(this, DeviceSelectionActivity.class);
@@ -82,22 +83,8 @@ public class HomeActivity extends AppCompatActivity {
         final ImageButton leaderboardButton = findViewById(R.id.leaderboardButton);
         final Button addDeviceButton = findViewById(R.id.itemButton);
 
-        final ScrollView scrollView = findViewById(R.id.scrollView);
         gridLayout = findViewById(R.id.gridLayout);
         context = this;
-
-
-//        final Button testButton = findViewById(R.id.testButton);
-//        testButton.setOnClickListener(new View.OnClickListener() {
-//            int count = 0;
-//            @Override
-//            public void onClick(View v) {
-//                Button newButton = new Button(HomeActivity.this);
-//                newButton.setText("Button " + count++);
-//                gridLayout.addView(newButton);
-//
-//            }
-//        });
 
 
         profileButton.setOnClickListener(new View.OnClickListener() {
@@ -107,7 +94,7 @@ public class HomeActivity extends AppCompatActivity {
                 Backend.getInstance().addSampleUsers(3);
 
                 // for testing purposes, clear the username as if the user has logged out
-                logout();
+                //logout();
 
                 startActivity(profileIntent);
             }
@@ -124,22 +111,9 @@ public class HomeActivity extends AppCompatActivity {
 
         //https://www.javatpoint.com/android-context-menu-example
         //Context Menu for delete device
-
         addDeviceButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // for testing purposes, adds a sample device:
-                //Device deviceToAdd = new Device(10, "Desktop", "Apple", "Mac Mini", 0.09);
-                //Backend.getInstance().addDevice(deviceToAdd);
-
-                // for testing purposes, returns a list of all devices
-                //ArrayList<Device> userDevices = Backend.getInstance().getDevices();
-//                Log.d(TAG, "about to print");
-//                for (Device d : userDevices) {
-//                    Log.d(TAG, d.toString());
-//                }
-
-                //Need to pass user so we can add device
                 startActivity(deviceIntent);
             }
         });
@@ -157,6 +131,7 @@ public class HomeActivity extends AppCompatActivity {
             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
                 Device d = dataSnapshot.getValue(Device.class);
                 devices.add(d);
+                addDeviceButton(d.getModel());
                 devicesUpdated();
             }
 
@@ -178,14 +153,8 @@ public class HomeActivity extends AppCompatActivity {
         });
     }
 
-    public static void setButtonParameters(Bundle bundle){
-        //company = bundle.getString(MODELNAME);
-        model = bundle.getString(MODELNAME);
-    }
-
-    public static void addDeviceButton(){
+    public static void addDeviceButton(String model){
         Button newButton = new Button(context);
-        System.out.println("Model: " + model);
         newButton.setText(model);
         gridLayout.addView(newButton);
     }
