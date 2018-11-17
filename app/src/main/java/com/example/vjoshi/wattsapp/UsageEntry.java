fccc259b9ff97489f760808147ffe8545b72e6cc
Vineet Joshi <vineet0713@gmail.com>
2018-11-17 13:47:14 -0800

    Add the start date to Usage Entries.

--- app/src/main/java/com/example/vjoshi/wattsapp/UsageEntry.java
+++ app/src/main/java/com/example/vjoshi/wattsapp/UsageEntry.java
@@ -4,15 +4,17 @@ import java.util.Date;
 
 public class UsageEntry {
     public String deviceType, deviceName;
+    public Date usageDate;
     public double wattsUsed;
 
     public UsageEntry() {
         // Default constructor required for calls to DataSnapshot.getValue(UsageEntry.class)
     }
 
-    public UsageEntry(String deviceType, String deviceName, double wattsUsed) {
+    public UsageEntry(String deviceType, String deviceName, Date usageDate, double wattsUsed) {
         this.deviceType = deviceType;
         this.deviceName = deviceName;
+        this.usageDate = usageDate;
         this.wattsUsed = wattsUsed;
     }
 
