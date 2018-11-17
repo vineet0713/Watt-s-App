5fa6a9dd7edd86115d8ba064960f07f7568633a8
Eric Castronovo <ecastronovo@scu.edu>
2018-11-17 13:52:46 -0800

    added code for piechart

--- app/src/main/java/com/example/vjoshi/wattsapp/TestPieGraph.java
+++ app/src/main/java/com/example/vjoshi/wattsapp/TestPieGraph.java
@@ -1,18 +1,91 @@
 package com.example.vjoshi.wattsapp;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+
+import java.util.ArrayList;
 
 public class TestPieGraph extends AppCompatActivity{
 
     private static String TAG = "TestPieGraph";
     private String[] devices = {"D1","D2","D3","D4","D5","D6"};
     private float[] watts = {10,2,13,43,13,85};
+
+
+    PieChart piechart;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_graphs);
         Log.d(TAG, "onCreate: Creating Pie Graph");
 
+        piechart = (PieChart) findViewById(R.id.PieChart);
+
+
+        piechart.setRotationEnabled(false);
+        //pieChart.setUsePercentValues(true);
+        //pieChart.setHoleColor(Color.BLUE);
+        //pieChart.setCenterTextColor(Color.BLACK);
+        piechart.setTransparentCircleAlpha(0);
+        piechart.setCenterText("Super Cool Chart");
+        piechart.setCenterTextSize(10);
+        piechart.setHoleRadius(0f);
+        //pieChart.setDrawEntryLabels(true);
+        //pieChart.setEntryLabelTextSize(20);
+        //More options just check out the documentation!
+
+        addDataSet();
+
+
+
+    }
+
+    private void addDataSet() {
+        Log.d(TAG, "addDataSet started");
+        ArrayList<PieEntry> yEntrys = new ArrayList<>();
+        ArrayList<String> xEntrys = new ArrayList<>();
+
+        for(int i = 0; i < watts.length; i++){
+            yEntrys.add(new PieEntry(watts[i] , i));
+        }
+
+        for(int i = 1; i < devices.length; i++){
+            xEntrys.add(devices[i]);
+        }
+
+        //create the data set
+        PieDataSet pieDataSet = new PieDataSet(yEntrys, "Employee Sales");
+        pieDataSet.setSliceSpace(2);
+        pieDataSet.setValueTextSize(12);
+
+        //add colors to dataset
+        ArrayList<Integer> colors = new ArrayList<>();
+        colors.add(Color.GRAY);
+        colors.add(Color.BLUE);
+        colors.add(Color.RED);
+        colors.add(Color.GREEN);
+        colors.add(Color.CYAN);
+        colors.add(Color.YELLOW);
+        colors.add(Color.MAGENTA);
+
+        pieDataSet.setColors(colors);
+
+//        //add legend to chart
+//        Legend legend = pieChart.getLegend();
+//        legend.setForm(Legend.LegendForm.CIRCLE);
+//        legend.setPosition(Legend.LegendPosition.LEFT_OF_CHART);
+//
+//        //create pie data object
+        PieData pieData = new PieData(pieDataSet);
+        piechart.setData(pieData);
+//        pieChart.invalidate();
     }
 }
