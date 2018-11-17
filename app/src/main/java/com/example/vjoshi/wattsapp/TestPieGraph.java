670a1a2f23905596df4b1be3889b6a55a373668b
Vineet Joshi <vineet0713@gmail.com>
2018-11-17 15:11:02 -0800

    Add functionality for displaying pie chart based on data retrieved from Firebase.

--- app/src/main/java/com/example/vjoshi/wattsapp/TestPieGraph.java
+++ app/src/main/java/com/example/vjoshi/wattsapp/TestPieGraph.java
@@ -10,14 +10,26 @@ import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class TestPieGraph extends AppCompatActivity{
-
     private static String TAG = "TestPieGraph";
-    private String[] devices = {"D1","D2","D3","D4","D5","D6"};
-    private float[] watts = {10,2,13,43,13,85};
+//    private String[] devices = {"D1","D2","D3","D4","D5","D6"};
+//    private float[] watts = {10,2,13,43,13,85};
 
+    // this is for testing purposes (to switch between grouping by devices, and grouping by device type)
+    private static final String PIE_CHART_TYPE = "DEVICES";
+//    private static final String PIE_CHART_TYPE = "DEVICE_TYPES";
+
+    private ArrayList<String> devices;
+    private ArrayList<Float> watts;
 
     PieChart piechart;
 
@@ -28,7 +40,6 @@ public class TestPieGraph extends AppCompatActivity{
         Log.d(TAG, "onCreate: Creating Pie Graph");
 
         piechart = (PieChart) findViewById(R.id.PieChart);
-
 
         piechart.setRotationEnabled(false);
         //pieChart.setUsePercentValues(true);
@@ -42,10 +53,50 @@ public class TestPieGraph extends AppCompatActivity{
         //pieChart.setEntryLabelTextSize(20);
         //More options just check out the documentation!
 
-        addDataSet();
+        devices = new ArrayList<>();
+        watts = new ArrayList<>();
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
 
+        loadData();
+    }
 
+    private void loadData() {
+        final DatabaseReference database = FirebaseDatabase.getInstance().getReference();
+        final String username = Backend.getInstance().getUsername();
+        database.child(username).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                User user = dataSnapshot.getValue(User.class);
+                ArrayList<UsageEntry> entries = user.getUsageEntries();
+                HashMap<String, Float> deviceToWatts = new HashMap<>();
+                for (UsageEntry entry : entries) {
+                    String device = (PIE_CHART_TYPE.equals("DEVICES")) ? entry.getDeviceName() : entry.getDeviceType();
+                    double entryWatts = entry.getWattsUsed();
+                    if (deviceToWatts.containsKey(device)) {
+                        entryWatts += deviceToWatts.get(device).doubleValue();
+                    }
+                    deviceToWatts.put(device, (float)(entryWatts));
+                }
+
+                devices.clear();
+                watts.clear();
+                for (String key : deviceToWatts.keySet()) {
+                    devices.add(key);
+                    watts.add(deviceToWatts.get(key));
+                }
+
+                piechart.invalidate();
+                // updates the pie chart with data
+                addDataSet();
+
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) { }
+        });
     }
 
     private void addDataSet() {
@@ -53,12 +104,12 @@ public class TestPieGraph extends AppCompatActivity{
         ArrayList<PieEntry> yEntrys = new ArrayList<>();
         ArrayList<String> xEntrys = new ArrayList<>();
 
-        for(int i = 0; i < watts.length; i++){
-            yEntrys.add(new PieEntry(watts[i] , i));
+        for(int i = 0; i < watts.size(); i++){
+            yEntrys.add(new PieEntry(watts.get(i).floatValue(), i));
         }
 
-        for(int i = 1; i < devices.length; i++){
-            xEntrys.add(devices[i]);
+        for(int i = 0; i < devices.size(); i++){
+            xEntrys.add(devices.get(i));
         }
 
         //create the data set
@@ -79,7 +130,7 @@ public class TestPieGraph extends AppCompatActivity{
         pieDataSet.setColors(colors);
 
 //        //add legend to chart
-//        Legend legend = pieChart.getLegend();
+//        Legend legend = piechart.getLegend();
 //        legend.setForm(Legend.LegendForm.CIRCLE);
 //        legend.setPosition(Legend.LegendPosition.LEFT_OF_CHART);
 //
